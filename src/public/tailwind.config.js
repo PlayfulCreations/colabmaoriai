6bd25d264fe3387fb2bda2181e40c6c79b6e04fa
@PlayfulCreations <148503106+PlayfulCreations@users.noreply.github.com>
2025-06-18 14:11:50 +1200

    Create tailwind.config.js

---
+++ src/public/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        accent: "#111e6c",
+        primary: "#f4f6f8",
+      },
+    },
+  },
+  plugins: [],
+};
